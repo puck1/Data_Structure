71da6bb0e759af40a57477d0c61887b38b89caa4
puck1 <31695863+puck1@users.noreply.github.com>
2018-04-11 16:44:16 +0800

    Delete tempCodeRunnerFile.c

--- Tree/tempCodeRunnerFile.c
+++
@@ -1,6 +0,0 @@
-    // printf("PostOrderThreading...");
-    // PostOrderThreading(Thrt);
-    // Sleep(500); printf("Succeeded!\n");
-    // printf("PostOrderTraverse_Thr:");
-    // PostOrderTraverse_Thr(Thrt,PrintElement);
-    // printf("\n");
