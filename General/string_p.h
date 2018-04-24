0c2a988354a5ea65ec51d026cbdaeced674bc52b
puck1 <31695863+puck1@users.noreply.github.com>
2018-04-25 00:58:40 +0800

    Add files via upload

---
+++ General/string_p.h
@@ -0,0 +1,34 @@
+/** @workspace/General/string_p.h
+ *  This file defines several functions to process string.
+ */
+
+#ifndef STRING_P_H
+#define STRING_P_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define INIT_LENGTH 10
+#define INCREASE_LENGTH 5
+
+char *getstr(void){
+    //从标准输入中，利用动态分配字符数组，读取不定长字符串
+    int i = 0 , len = INIT_LENGTH;
+    char e;
+    char *tmp;
+    tmp = (char *)malloc((INIT_LENGTH * sizeof(char)));
+    e = getchar();
+    while(e != '\n'){
+        if(i >= len - 1){
+            len += INCREASE_LENGTH;
+            tmp = (char *)realloc(tmp,len * sizeof(char));
+        }
+        tmp[i++] = e;
+        e = getchar();
+    }
+    tmp = (char *)realloc(tmp,(i + 1) * sizeof(char));
+    tmp[i] = '\0';
+    return tmp;
+}//getstr
+
+#endif // !STRING_P_H
